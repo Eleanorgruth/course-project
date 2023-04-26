e7c17a8166d83552c9644417e1d3bcbf7d817124
Eleanor Ruth <ellie.ruth@me.com>
2023-04-26 15:28:03 -0600

    Initial commit

---
+++ src/app/recipe-component/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeList {
+ recipes: Recipe[] = [
+  new Recipe("pizza", "pizza description", "https://img.freepik.com/free-photo/thinly-sliced-pepperoni-is-popular-pizza-topping-american-style-pizzerias-isolated-white-background-still-life_639032-229.jpg?size=626&ext=jpg&ga=GA1.1.1510911396.1682543127&semt=robertav1_2_sidr"),
+  new Recipe("pasta", "pasta description", "https://img.freepik.com/free-photo/thinly-sliced-pepperoni-is-popular-pizza-topping-american-style-pizzerias-isolated-white-background-still-life_639032-229.jpg?size=626&ext=jpg&ga=GA1.1.1510911396.1682543127&semt=robertav1_2_sidr"),
+ ]
+}
